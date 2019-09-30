92d6651355bc0c58ac274283cfb7b5c91ab0b72f
Luiz A D Silva <luizbha@hotmail.com>
2019-09-30 16:58:34 -0300

    Primeiro teste Laravel

---
+++ resources/views/hello2.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>
+        </title>
+    </head> 
+
+    <body>
+        <h1>Hello World 2 - teste view hello2 <?php echo(2 + 2 ) ?> </h1>
+    </body>
+
+</html>    
+
